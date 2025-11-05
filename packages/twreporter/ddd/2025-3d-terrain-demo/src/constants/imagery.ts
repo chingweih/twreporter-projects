3e99d375424d33391ac9aef55bb5386ad408993b
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2025-11-05 11:50:11 +0800

    chore(2025-3d-terrain-demo): add google satellite imagery

--- packages/twreporter/ddd/2025-3d-terrain-demo/src/constants/imagery.ts
+++ packages/twreporter/ddd/2025-3d-terrain-demo/src/constants/imagery.ts
@@ -2,6 +2,7 @@ export const source = {
   nlsc: 'https://wmts.nlsc.gov.tw/wmts/PHOTO2/default/GoogleMapsCompatible/{z}/{y}/{x}',
   arcGIS:
     'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+  google: 'https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}',
   selfhostedBaseUrl:
     'https://storage.googleapis.com/data-reporter-infographics/dev/2025-3d-terrain-demo/tiles',
 }
