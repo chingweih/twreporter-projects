37c88ec3fe164ce60f56be74b2362ac880a8a671
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2025-11-05 11:49:50 +0800

    feat(2025-3d-terrain): adjust loading styling

--- packages/twreporter/ddd/2025-3d-terrain-demo/src/components/Loading.svelte
+++ packages/twreporter/ddd/2025-3d-terrain-demo/src/components/Loading.svelte
@@ -1,12 +1,27 @@
-<img
-  src="https://www.twreporter.org/images/spinner-logo.gif"
-  alt="資料載入中⋯⋯"
-  class="loading-spinner"
-/>
+<div class="loading">
+  <img
+    src="https://www.twreporter.org/images/spinner-logo.gif"
+    alt="資料載入中⋯⋯"
+    class="loading-spinner"
+  />
+  <p>載入中⋯</p>
+</div>
 
 <style>
+  .loading {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
+  }
+
   .loading-spinner {
-    width: 100px;
+    width: 50px;
     margin: auto;
+  }
+
+  p {
+    color: white;
   }
 </style>
