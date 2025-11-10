41146bbd598a59c8394c74482b1c4194266a5fba
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2025-11-10 16:55:30 +0800

    chore(2025-3d-terrain-demo): add carto

--- packages/twreporter/ddd/2025-3d-terrain-demo/src/constants/imagery.ts
+++ packages/twreporter/ddd/2025-3d-terrain-demo/src/constants/imagery.ts
@@ -3,6 +3,8 @@ export const source = {
   arcGIS:
     'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
   google: 'https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}',
+  carto:
+    'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png',
   selfhostedBaseUrl:
     'https://storage.googleapis.com/data-reporter-infographics/dev/2025-3d-terrain-demo/tiles',
 }
