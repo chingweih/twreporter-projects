565a53b78f543123804827b696d1ed84880088f5
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-02-22 22:08:46 +0800

    feat(2026-03-baseball): draft background scroller with example component

---
+++ packages/twreporter/ddd/2026-03-baseball/src/wc/ScrollDiagram.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import { ScrollerBase } from "@reuters-graphics/graphics-components";
+
+    // Optional: Bind your own variables to use them in your code.
+    let count = $state(1);
+    let index = $state(0);
+    let offset = $state(0);
+    let progress = $state(0);
+    let top = $state(0.1);
+    let threshold = $state(0.5);
+    let bottom = $state(0.9);
+</script>
+
+<ScrollerBase
+    {top}
+    {threshold}
+    {bottom}
+    bind:count
+    bind:index
+    bind:offset
+    bind:progress
+    query="div.step-foreground-container"
+>
+    {#snippet backgroundSnippet()}
+        <!-- Add custom background HTML or component -->
+        <p class="mb-0">
+            Current step: <strong>{index + 1}/{count}</strong>
+        </p>
+        <progress class="mb-4" value={(index + 1) / count}></progress>
+
+        <p class="mb-0">Offset in current step</p>
+        <progress class="mb-4" value={offset}></progress>
+
+        <p class="mb-0">Total progress</p>
+        <progress class="mb-4" value={progress}></progress>
+    {/snippet}
+    {#snippet foregroundSnippet()}
+        <!-- Add custom foreground HTML or component -->
+        <div class="step-foreground-container">Step 1</div>
+        <div class="step-foreground-container">Step 2</div>
+        <div class="step-foreground-container">Step 3</div>
+        <div class="step-foreground-container">Step 4</div>
+        <div class="step-foreground-container">Step 5</div>
+    {/snippet}
+</ScrollerBase>
+
+<style lang="scss">
+    .step-foreground-container {
+        height: 100vh;
+        width: 50%;
+        background-color: rgba(0, 0, 0, 0.2);
+        padding: 1em;
+        margin: 0 0 2em 0;
+        position: relative;
+        left: 50%;
+    }
+</style>
--- packages/twreporter/ddd/2026-03-baseball/src/wc/ScrollDiagram.wc.svelte
+++ packages/twreporter/ddd/2026-03-baseball/src/wc/ScrollDiagram.wc.svelte
@@ -2,13 +2,7 @@
 <svelte:options customElement={{ tag: "twreporter-scroll-diagram" }} />
 
 <script lang="ts">
-    import Shell from "../components/Shell.svelte";
     import ScrollDiagram from "./ScrollDiagram.svelte";
-
-    const name = "scroll diagram";
-    const footnotes = ["註：1234"];
 </script>
 
-<Shell {name} {footnotes}>
-    <ScrollDiagram />
-</Shell>
+<ScrollDiagram />
