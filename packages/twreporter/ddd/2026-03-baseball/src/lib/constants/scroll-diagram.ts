15cfcb94970d594dbc768441493386f8a163a5b1
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-03-05 20:25:06 +0800

    feat(2026-03-baseball): update steps

--- packages/twreporter/ddd/2026-03-baseball/src/lib/constants/scroll-diagram.ts
+++ packages/twreporter/ddd/2026-03-baseball/src/lib/constants/scroll-diagram.ts
@@ -139,39 +139,34 @@ export const nodes: NodeMeta[] = [
 ];
 
 export const steps: ScrollStep[] = [
-  // ── 分鏡一 ──
-  // 吉力吉撈・鞏冠 > Zoom out > 影視配樂 > 平移 > 現代流行樂
+  // Section 1
+  { to: "node__", path: "direct" },
   {
     to: "node__1-b-1",
     path: "direct",
     text: "從吉力吉撈・鞏冠的「西部牛仔」風到大師兄林智勝《少林足球》主題曲、林立《洛克人2》電玩配樂，影視與動漫、電玩一直是台灣應援曲的靈感來源大宗。",
     audios: [
       {
-        src: "https://projects.twreporter.org/twreporter/ddd/2026-03-baseball/audios/D01/01.mp3",
-        name: "味全龍「吉力吉撈・鞏冠」應援曲",
-      },
-      {
         src: "https://projects.twreporter.org/twreporter/ddd/2026-03-baseball/audios/D01/02.mp3",
         name: "統一獅「林安可」應援曲",
       },
     ],
+    scale: 1,
   },
-  { to: "node__1", path: "direct" },
   { to: "node__2", path: "direct" },
 
-  // ── 分鏡二 ──
-  // 現代流行樂 > 平移 > Zoom in > 世界音樂
+  // Section 2
   {
-    to: "node__2",
+    to: "node__3",
     path: "direct",
     text: "其實台灣應援曲中，不只有本土元素，還藏著世界各文化圈獨有的音色、節奏、音程結構。",
   },
-  { to: "node__3", path: "direct" },
+  { to: "node__3", path: "direct", scale: 0.5 },
+  { to: "node__4", path: "direct" },
 
-  // ── 分鏡三 ──
-  // 世界音樂 > Zoom out > 平移 > 古典
+  // Section 3
   {
-    to: "node__3",
+    to: "node__4-a",
     path: "direct",
     text: "進一步以「樂種」進行分類，台灣重砲手們，以及歷史悠久的中信兄弟，依然保持著從古典樂延伸的進行曲、軍歌風格。",
     audios: [
@@ -181,33 +176,26 @@ export const steps: ScrollStep[] = [
       },
     ],
   },
-  { to: "node__3", path: "direct", scale: 0.5 },
-  { to: "node__4", path: "direct", scale: 0.5 },
 
-  // ── 分鏡四 ──
-  // 古典 > 平移 > 藍調 > 平移 > 鄉村
-  {
-    to: "node__4",
-    path: "direct",
-    text: "構成台灣應援曲的一大根基，還有職棒早期大量由歐美民謠、童謠改編的「傳統應援」。",
-  },
-  { to: "node__6", path: "direct" },
-  { to: "node__5", path: "direct" },
-
-  // ── 分鏡五 ──
-  // 鄉村 > 平移 > zoom out > 搖滾一覽
+  // Section 4
   {
     to: "node__5",
     path: "direct",
+    text: "構成台灣應援曲的一大根基，還有職棒早期大量由歐美民謠、童謠改編的「傳統應援」。",
+  },
+
+  // Section 5
+  {
+    to: "node__6",
+    path: "direct",
     text: "美國「藍調音樂」身為現代流行樂種的根源，自然也是棒球應援曲的重要支派，尤其在台灣進入「現代應援」時期後，大量以電吉他編曲的音樂，成為當代棒球場的核心聲響。",
   },
-  { to: "node__5", path: "direct", scale: 0.5 },
-  { to: "node__6-a", path: "direct", scale: 0.5 },
+  { to: "node__6", path: "direct" },
+  { to: "node__6-b", path: "direct" },
 
-  // ── 分鏡六 ──
-  // 搖滾一覽 > 平移 > 放克 > 迪斯可 > 電子
+  // Section 6
   {
-    to: "node__6-a",
+    to: "node__6-b-i-1-a",
     path: "direct",
     text: "近年球場硬體設備提升，應援又有舞蹈需求，以電子樂構成主旋律，融合電吉他、爵士鼓加強熱血感的樂曲也開始出現。",
     audios: [
@@ -217,7 +205,4 @@ export const steps: ScrollStep[] = [
       },
     ],
   },
-  { to: "node__6-b", path: "direct" },
-  { to: "node__6-b-i", path: "direct" },
-  { to: "node__6-b-ii", path: "direct" },
 ];
