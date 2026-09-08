859ac826526239cad89f1c1ac641d16ec648d058
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-09-08 12:29:23 +0800

    fix(2026-07-kodokushi): generate schema inside js folder

--- packages/twreporter/ddd/2026-07-kodokushi/vite.config.ts
+++ packages/twreporter/ddd/2026-07-kodokushi/vite.config.ts
@@ -7,16 +7,22 @@ const timestamp = Date.now()
 
 export default defineConfig({
   publicDir: 'src/lib/assets',
-  plugins: [{
-    name: 'kodokushi-schema',
-    generateBundle() {
-      this.emitFile({
-        type: 'asset',
-        fileName: 'kodokushi.schema.json',
-        source: JSON.stringify(z.toJSONSchema(configSchema, { target: 'draft-7' }), null, 2),
-      })
+  plugins: [
+    {
+      name: 'kodokushi-schema',
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: 'js/kodokushi.schema.json',
+          source: JSON.stringify(
+            z.toJSONSchema(configSchema, { target: 'draft-7' }),
+            null,
+            2,
+          ),
+        })
+      },
     },
-  }],
+  ],
   server: {
     cors: {
       origin: '*',
